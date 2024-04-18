0f6f238ecdc3b5b32b5c21aeac331bc3123a6fa7
mohammad <kasifsaif784@gmail.com>
2024-04-18 12:52:44 +0530

    changs

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,10 @@ process.on('uncaughtException', (err) => {
 
 
 
-// if(process.env.NODE_ENV !== 'production'){
-//   app.use(express.static("frontend/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-//   });
-// }
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+}
+
 
 // Server runnig 
 const server = app.listen(process.env.PORT, () => {
